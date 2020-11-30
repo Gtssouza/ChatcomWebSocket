4c637231e990c3e15756c5554c22ccc686e4abee
Gustavo Souza <gtssouza94@gmail.com>
2020-11-30 10:27:09 -0300

    Recebendo e validando os dados do formulario de entrada

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,14 @@
 module.exports.initChat = function(application, req, res){
-    res.render('chat');
+    var dadosForm = req.body;
+    
+    req.assert('apelido', 'Nome ou apelido obrigatorio').notEmpty();
+    req.assert('apelido','O campo deve ter entre 3 e 15 caracteres').len(3,15);
+
+    var erros = req.validationErrors();
+
+    if(erros){
+        res.render('index.ejs', {validacao :erros});
+        return;
+    }
+    res.render('chat.ejs');
 }
